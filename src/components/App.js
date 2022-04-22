4ee96415ebf13f13b2e840aa89c39114089960d8
444vhenvhen <104181148+444vhenvhen@users.noreply.github.com>
2022-04-22 13:15:43 +0800

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from "./header";
+import AddContact from "./AddContact";
+import contactList from "./contactList";
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* <AddContact />
+      <contactList /> */}
+    </div>
+    );
+}
+
+export default App;
